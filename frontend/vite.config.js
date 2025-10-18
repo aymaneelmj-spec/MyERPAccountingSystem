d6e8d331b9a8d8de04ceac4ebe79dcd119dfc610
aymaneelmj-spec <aymaneelmj@gmail.com>
2025-10-18 06:25:51 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
+ 
 })
